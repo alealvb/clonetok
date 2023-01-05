222b10fc6d72a0a0c7b1c651fa0d040797fb7258
Alejandro Alvarez <alejandro.alvarez.coder@gmail.com>
2023-01-05 20:31:34 -0300

    remove app service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from 'src/auth/auth.module';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { UsersModule } from 'src/users/users.module';
-import { AppService } from './app.service';
 
 @Module({
   imports: [AuthModule, UsersModule],
   providers: [
-    AppService,
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
