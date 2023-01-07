dc5b7545d766e0d44da6b5b4e355b892a8c7bf07
Alejandro Alvarez <alejandro.alvarez.coder@gmail.com>
2023-01-07 00:18:31 -0300

    add post module to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from 'src/auth/auth.module';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { PostModule } from 'src/post/post.module';
 import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, PostModule],
   providers: [
     {
       provide: APP_GUARD,
